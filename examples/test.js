8abd43cb665d371682a48d758e7070a08af730a3
dr-js <dr@dr.run>
2020-03-29 17:22:22 +0800

    CHG: add test & packaging

--- examples/test.js
+++ examples/test.js
@@ -1,92 +1,17 @@
 import { readFileSync } from 'fs'
 import { resolve } from 'path'
-import SVGO from '../lib/svgo'
+import { SVGO } from '../lib/svgo'
 
 const filepath = resolve(__dirname, 'test.svg')
-const svgo = new SVGO({
-  plugins: [ {
-    cleanupAttrs: true
-  }, {
-    removeDoctype: true
-  }, {
-    removeXMLProcInst: true
-  }, {
-    removeComments: true
-  }, {
-    removeMetadata: true
-  }, {
-    removeTitle: true
-  }, {
-    removeDesc: true
-  }, {
-    removeUselessDefs: true
-  }, {
-    removeEditorsNSData: true
-  }, {
-    removeEmptyAttrs: true
-  }, {
-    removeHiddenElems: true
-  }, {
-    removeEmptyText: true
-  }, {
-    removeEmptyContainers: true
-  }, {
-    removeViewBox: false
-  }, {
-    cleanupEnableBackground: true
-  }, {
-    convertStyleToAttrs: true
-  }, {
-    convertColors: true
-  }, {
-    convertPathData: true
-  }, {
-    convertTransform: true
-  }, {
-    removeUnknownsAndDefaults: true
-  }, {
-    removeNonInheritableGroupAttrs: true
-  }, {
-    removeUselessStrokeAndFill: true
-  }, {
-    removeUnusedNS: true
-  }, {
-    cleanupIDs: true
-  }, {
-    cleanupNumericValues: true
-  }, {
-    moveElemsAttrsToGroup: true
-  }, {
-    moveGroupAttrsToElems: true
-  }, {
-    collapseGroups: true
-  }, {
-    removeRasterImages: false
-  }, {
-    mergePaths: true
-  }, {
-    convertShapeToPath: true
-  }, {
-    sortAttrs: true
-  }, {
-    removeDimensions: true
-  }, {
-    removeAttrs: { attrs: '(stroke|fill)' }
-  } ]
-})
-
 const data = readFileSync(filepath, 'utf8')
-svgo.optimize(data, { path: filepath })
-  .then(function (result) {
-    console.log(result)
 
-    // {
-    //     // optimized SVG data string
-    //     data: '<svg width="10" height="20">test</svg>'
-    //     // additional info such as width/height
-    //     info: {
-    //         width: '10',
-    //         height: '20'
-    //     }
-    // }
-  })
+const svgo = new SVGO()
+svgo.optimize(data, { path: filepath })
+  .then(console.log)
+// {
+//   // optimized SVG data string
+//   data: '<svg width="10" height="20">test</svg>',
+//   // additional info such as width/height
+//   info: { width: '10', height: '20' },
+//   path: '.../test.svg'
+// }
---
+++ script/index.js
@@ -0,0 +1,60 @@
+import { resolve } from 'path'
+import { execSync } from 'child_process'
+
+import { getScriptFileListFromPathList } from '@dr-js/dev/module/node/file'
+import { initOutput, packOutput, verifyGitStatusClean, publishOutput } from '@dr-js/dev/module/output'
+import { getTerserOption, minifyFileListWithTerser } from '@dr-js/dev/module/minify'
+import { processFileList, fileProcessorBabel, fileProcessorWebpack } from '@dr-js/dev/module/fileProcessor'
+import { runMain, argvFlag } from '@dr-js/dev/module/main'
+
+const PATH_ROOT = resolve(__dirname, '..')
+const PATH_OUTPUT = resolve(__dirname, '../output-gitignore')
+const fromRoot = (...args) => resolve(PATH_ROOT, ...args)
+const fromOutput = (...args) => resolve(PATH_OUTPUT, ...args)
+const execShell = (command) => execSync(command, { cwd: fromRoot(), stdio: argvFlag('quiet') ? [ 'ignore', 'ignore', 'inherit' ] : 'inherit' })
+
+const buildOutput = async ({ isTest, logger }) => {
+  logger.padLog('build library')
+  execShell('npm run build-library-lib')
+  execShell('npm run build-library-plugins')
+  logger.padLog('build module')
+  execShell('npm run build-module-lib')
+  execShell('npm run build-module-plugins')
+  logger.padLog('build webpack')
+  execShell('npm run build-webpack')
+  isTest && logger.padLog('build test')
+  isTest && execShell('npm run build-library-test')
+  isTest && execShell('npm run build-module-test')
+}
+
+const processOutput = async ({ logger }) => {
+  const fileListLibrary = await getScriptFileListFromPathList([ 'lib', 'plugins' ], fromOutput)
+  const fileListModule = await getScriptFileListFromPathList([ 'module' ], fromOutput)
+  const fileListBabel = [ ...fileListLibrary, ...fileListModule ]
+  const fileListWebpack = await getScriptFileListFromPathList([ 'webpack' ], fromOutput)
+  let sizeReduce = 0
+  sizeReduce += await minifyFileListWithTerser({ fileList: fileListLibrary, option: getTerserOption(), rootPath: PATH_OUTPUT, logger })
+  sizeReduce += await minifyFileListWithTerser({ fileList: fileListModule, option: getTerserOption({ isReadable: true }), rootPath: PATH_OUTPUT, logger })
+  sizeReduce += await processFileList({ fileList: fileListBabel, processor: fileProcessorBabel, rootPath: PATH_OUTPUT, logger })
+  sizeReduce += await minifyFileListWithTerser({ fileList: fileListWebpack, option: getTerserOption(), rootPath: PATH_OUTPUT, logger })
+  sizeReduce += await processFileList({ fileList: fileListWebpack, processor: fileProcessorWebpack, rootPath: PATH_OUTPUT, logger })
+  logger.log(`total size reduce: ${sizeReduce}B`)
+}
+
+runMain(async (logger) => {
+  const packageJSON = await initOutput({ fromRoot, fromOutput, copyPathList: [ 'README.md', 'LICENSE' ], pathLicenseFile: false, logger })
+  if (!argvFlag('pack')) return
+  const isTest = argvFlag('test', 'publish', 'publish-dev')
+  await buildOutput({ isTest, logger })
+  await processOutput({ logger })
+  isTest && logger.padLog('lint source')
+  isTest && execShell('npm run lint')
+  isTest && await processOutput({ logger }) // once more
+  isTest && logger.padLog('test output')
+  isTest && execShell('npm run test-output-library')
+  isTest && execShell('npm run test-output-module')
+  isTest && execShell('npm run script-clear-output-test')
+  isTest && await verifyGitStatusClean({ fromRoot, logger })
+  const pathPackagePack = await packOutput({ fromRoot, fromOutput, logger })
+  await publishOutput({ flagList: process.argv, packageJSON, pathPackagePack, logger })
+})
